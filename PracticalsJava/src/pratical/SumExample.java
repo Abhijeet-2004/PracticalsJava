8c978279e7cef41d831a263002d9dd9763b39e39
Abhijeet-2004 <abhijeetsahoo344@gmail.com>
2023-05-07 10:40:48 +0530

    Some modification in comment of program-5

--- PracticalsJava/src/pratical/SumExample.java
+++ PracticalsJava/src/pratical/SumExample.java
@@ -1,4 +1,4 @@
-/*To find the sum of any number of integers interactively ,i.e ., entering evey number from the keyboard ,
+/*To find the sum of any number of integers interactively ,i.e ., entering every number from the keyboard ,
 whereas the total number of integers is given as a command line argument : Program-5 */
 package pratical;
 import java.util.Scanner;
