25c27dc7ae8e4b4228539889c3013c97fd15e105
Abhijeet sahoo <124451670+Abhijeet-2004@users.noreply.github.com>
2023-05-11 00:52:16 +0530

    program 12 added

---
+++ PracticalsJava/src/pratical/BoxingAndUnboxing.java
@@ -0,0 +1,15 @@
+//. Write a program to demonstrate the concept of boxing and unboxing : program -12
+package pratical;
+public class BoxingAndUnboxing {
+public static void main(String[] args) {
+	//Boxing : converting primitive data type to wrapper object
+	int num=21;
+	@SuppressWarnings("removal")
+	Integer boxNumber=new Integer(num);
+	System.out.println("Boxing:"+boxNumber);
+	
+	//Unboxing : converting wrapper object to primitive data type 
+	int unBoxNumber=boxNumber;
+	System.out.println("unboxing:"+unBoxNumber);
+}
+}
