fb1e6ffb0e48cee39b812e66ad1c17693375627a
Abhijeet sahoo <124451670+Abhijeet-2004@users.noreply.github.com>
2023-05-11 02:13:47 +0530

    program 14 added

--- PracticalsJava/src/pratical/MultiLevelPackUse.java
+++ PracticalsJava/src/pratical/MultiLevelPackUse.java
@@ -4,7 +4,7 @@ import subSubPackage.Fibonacci;
 public class MultiLevelPackUse {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter number of fibonacci number you want to generate :");
+		System.out.println("Enter number of fibonacci number you want to generate:");
 		Fibonacci.FibonaciGenerator(sc.nextInt());
 		sc.close();
 	}
