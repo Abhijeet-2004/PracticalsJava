59cdc6d3caa462a73c79831739966e7f7d55d42b
Abhijeet sahoo <124451670+Abhijeet-2004@users.noreply.github.com>
2023-05-11 13:25:37 +0530

    program 14 added

--- PracticalsJava/src/pratical/MultiLevelPackUse.java
+++ PracticalsJava/src/pratical/MultiLevelPackUse.java
@@ -1,6 +1,9 @@
+/*Write a program to create a multilevel package and also creates a reusable class to 
+generate Fibonacci series, where the function to generate Fibonacci series is given in 
+a different file belonging to the same package. program-14 */
 package pratical;
 import java.util.Scanner;
-import subSubPackage.Fibonacci;
+import pratical.SubPackage.SubSubPackage.Fibonacci;
 public class MultiLevelPackUse {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
---
+++ PracticalsJava/src/pratical/SubPackage/SubSubPackage/Fibonacci.java
@@ -0,0 +1,20 @@
+package pratical.SubPackage.SubSubPackage;
+
+	public class Fibonacci {
+		public static void FibonaciGenerator(int n) {
+			System.out.println("First "+n+" fibonacci numberes are :");
+			for(int i=0;i<n;i++) {
+				System.out.print(fibo(i)+" ");
+			}
+		}
+		private static int fibo(int num) {
+			if(num==0)
+				return 0;
+			else if(num==1)
+				return 1;
+			else 
+				return fibo(num-1)+fibo(num-2);
+		}
+	}
+
+
